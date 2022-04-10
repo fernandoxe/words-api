acb14797d7d276b92ac680d10963c661700c01a7
Fernando Parra <fernandoxe@gmail.com>
2022-04-10 16:36:42 -0300

    chore: deleted

--- api/swiftdle/index.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getTodayWord } from '../../src/services';
-import { words } from '../../data/taylor';
-
-export default (_req: VercelRequest, res: VercelResponse) => {
-  const todayWord = getTodayWord(words);
-  res.json(todayWord);
-};
