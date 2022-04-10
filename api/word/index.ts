07fe4152360ad40ef66a62a9a67b7bcabfb01ee7
Fernando Parra <fernandoxe@gmail.com>
2022-04-10 16:37:37 -0300

    chore: word

---
+++ api/word/index.ts
@@ -0,0 +1,8 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { getTodayWord } from '../../src/services';
+import { words } from '../../data/words';
+
+export default (_req: VercelRequest, res: VercelResponse) => {
+  const todayWord = getTodayWord(words);
+  res.json(todayWord);
+};
---
+++ api/word/random/index.ts
@@ -0,0 +1,8 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { getRandomWord } from '../../../src/services';
+import { words } from '../../../data/words';
+
+export default (_req: VercelRequest, res: VercelResponse) => {
+  const randomWord = getRandomWord(words);
+  res.json(randomWord);
+};
