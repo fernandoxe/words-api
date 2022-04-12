c7d9d342bd152d1a79b2b8bbe12e6f344281bdb8
Fernando Parra <fernandoxe@gmail.com>
2022-04-11 21:31:16 -0300

    chore: words constants

---
+++ src/constants/words/index.ts
@@ -0,0 +1,40 @@
+export const APP_NAME = 'Words';
+
+export const APP_SHORT_URL = 'words.com';
+
+export const APP_URL = `https://${APP_SHORT_URL}`;
+
+export const APP_TODAY_TEXT = `A new ${APP_NAME} is available!\n\nPlay it on ${APP_SHORT_URL}`;
+
+export const APP_YESTERDAY_MESSAGE = `Here is yesterday's ${APP_NAME}`;
+
+export const IMAGE_WIDTH = 1080;
+export const IMAGE_HEIGHT = 1080;
+
+export const IMAGES_FOLDER = 'words';
+export const BACKGROUND_COLOR = '#0f172a';
+
+export const PADDING = 80;
+export const MARGIN = 40;
+
+export const FONT_COLOR = '#f5f5f5';
+export const FONT_COLOR_WORD = '#c084fc';
+// export const FONT_COLOR_WORD = '#9333ea';
+
+export const TEXT_ALIGN = 'center';
+
+export const TEXT_BASELINE = 'middle';
+
+export const SHADOW_COLOR = 'black';
+export const SHADOW_BLUR = 4;
+export const SHADOW_OFFSETX = 4;
+export const SHADOW_OFFSETY = 4;
+
+export const FONT_SIZE = 40;
+export const FONT_SIZE_TITLE = 60;
+export const FONT_SIZE_WORD = 160;
+export const FONT_SIZE_LINE = 60;
+
+export const FONT_NAME = 'Nunito';
+
+export const IMAGE_MIME_TYPE = 'image/jpeg';
