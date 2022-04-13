eb6c71f4260bc1d19dd7ea34334501d50525e641
Fernando Parra <fernandoxe@gmail.com>
2022-04-13 20:27:33 -0300

    chore: moved

--- api/date/index.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default (_req: VercelRequest, res: VercelResponse) => {
-  const date = new Date();
-  res.json({ date: date.toLocaleString() });
-};
